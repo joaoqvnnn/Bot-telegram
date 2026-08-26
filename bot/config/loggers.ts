8f0815f6d8ee5b72032aafc28ebaf54b1c0ebafe
joaoqvnnn <empresadefofocas@gmail.com>
2026-08-26 20:18:10 -0300

    Add logger utility with info, error, and warn methods

---
+++ bot/config/loggers.ts
@@ -0,0 +1,11 @@
+export const logger = {
+  info: (mensagem: string, ...args: any[]) => {
+    console.log(`[INFO] ${mensagem}`, ...args);
+  },
+  error: (mensagem: string, ...args: any[]) => {
+    console.error(`[ERROR] ${mensagem}`, ...args);
+  },
+  warn: (mensagem: string, ...args: any[]) => {
+    console.warn(`[WARN] ${mensagem}`, ...args);
+  },
+};
